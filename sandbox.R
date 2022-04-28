9f55351b8e3a9df0445bfd79e457b4ca585e9bab
Kaija <kaija.gahm@gmail.com>
2022-04-28 13:26:56 -0700

    got the network plots with consistent coordinates working

--- sandbox.R
+++ sandbox.R
@@ -2,8 +2,8 @@
 
 # n = number of individuals
 # m = number of individuals who die (mortality)
-# r = proportion of dyads removed per day
-# c = proportion of dyads created per day
+# remove = proportion of dyads removed per day
+# create = proportion of dyads created per day
 # Removal and connection should depend just on the previous day (maybe later we can allow memory beyond that)
 
 library(igraph)
@@ -11,8 +11,8 @@ library(tidyverse)
 n <- 30
 density <- 0.15
 seed <- 3
-r <- 0.1
-c <- 0.05
+remove <- 0.1
+create <- 0.05
 days <- 10
 
 set.seed(seed)
@@ -30,7 +30,7 @@ coords <- layout_with_fr(init_graph) %>%
   bind_cols(tibble(names = names(V(init_graph))))
 
 # Loss and gain of edges --------------------------------------------------
-generateAMs <- function(n, density, seed, r, c, days){
+generateNetworks <- function(n, density, seed, remove, create, days){
   # Set the seed
   set.seed(seed)
   initialConnections <- sample(0:1, size = n*n, replace = T, prob = c(1-density, density))
@@ -38,7 +38,7 @@ generateAMs <- function(n, density, seed, r, c, days){
   # Create the initial adjacency matrix
   am_init <- matrix(data = initialConnections, nrow = n, ncol = n)
 
-  # For each day, remove r connections and create c connections
+  # For each day, remove `remove` connections and create `create` connections
   ams <- vector(mode = "list", length = days) # create a list to store the adjacency matrices
   ams[[1]] <- am_init
   for(i in 2:days){
@@ -47,21 +47,34 @@ generateAMs <- function(n, density, seed, r, c, days){
     for(col in 1:ncol(mat_prev)){
       for(row in 1:nrow(mat_prev)){
         if(mat_prev[row, col] == 0){
-          mat_new[row, col] <- sample(0:1, size = 1, replace = TRUE, prob = c(1-c, c))
+          mat_new[row, col] <- sample(0:1, size = 1, replace = TRUE, 
+                                      prob = c(1-create, create))
         }else{
-          mat_new[row, col] <- sample(0:1, size = 1, replace = TRUE, prob = c(1-r, r))
+          mat_new[row, col] <- sample(0:1, size = 1, replace = TRUE, 
+                                      prob = c(1-remove, remove))
         }
       }
     }
     ams[[i]] <- mat_new # assign new matrix to the corresponding place in the list.
   }
   
-  return(ams)
+  # Make the adjacency matrices into graphs
+  graphs <- lapply(ams, graph_from_adjacency_matrix, weighted = NULL, mode = "undirected", diag = FALSE)
+  
+  # Return both the adjacency matrices and the graphs
+  return(list("ams" = ams, "graphs" = graphs))
 }
 
-ams <- generateAMs(n = 20, density = 0.1, seed = 3, r = 0.1, c = 0.05, days = 10)
-graphs <- lapply(ams, graph_from_adjacency_matrix, weighted = NULL, mode = "undirected", diag = FALSE)
-lapply(graphs, function(x){
-  plot(x, vertex.size = .8, edge.arrow.size=.4, vertex.label = NA, 
-       layout = as.matrix(coords[,1:2]), rescale=F)
+# Create random coords for the nodes, which will be consistent when we plot
+coords <- layout_with_fr(networks$graphs[[1]])
+
+networks <- generateNetworks(n = 20, density = 0.1, seed = 3, remove = 0.1, create = 0.05, days = 10)
+
+# Plot all the networks sequentially, individually
+networkPlots <- lapply(networks$graphs, function(x){
+  plot(x, layout = coords[,1:2], rescale=T)
 })
+
+# Plot all the networks as a facetted plot
+
+cowplot::plot_grid()
