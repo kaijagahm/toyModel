d775e38c177ec2e88000e4692fb6d854a1fc2d69
Kaija Gahm <kgahm@ucla.edu>
2022-07-06 14:38:53 -0700

    revised for beta distrs and other params

--- modelCode_rewiring.R
+++ modelCode_rewiring.R
@@ -1,11 +1,7 @@
 # Model code, version 2
 # Architecture of this version is based more directly on Farine's 2nd-degree rewiring model (2021)
 # AUTHOR: Kaija Gahm (kgahm@ucla.edu)
-# REVISION DATE: 2022-05-14
-
-# Code overview -----------------------------------------------------------
-# 1. random starting network
-# 2. burn-in period. This is where the baseline dynamics come in. For Farine, the baseline dynamics were the Ilany & Akcay model of social inheritance. For me, the equivalent burn-in will be the random network fluctuations, without adding or removing any nodes. Edges get removed and added based on probabilities.
+# REVISION DATE: 2022-07-06 (added parameters derived in parameterizingTheModel.Rmd)
 
 # Load packages -----------------------------------------------------------
 library(tidyverse)
@@ -25,7 +21,7 @@ source("supportingFunctions.R") # all the functions that will be used in the mai
 N <- 50 # Nodes in the network
 nodes.removed <- 1 # Nodes to remove
 n.removed <- 1 # How many to remove at a time
-edge.prob <- 0.1 # initial probability of edges in the random starting network
+edge.prob <- 0.04 # This is derived from the average network density, when taking a 5-day increment, from parameterizingTheModel.Rmd.
 
 # Simulation parameters
 n.rep <- 100 # number of repetitions
@@ -34,19 +30,11 @@ burn.out <- 50 # number of days to continue after the perturbation
 pm <- 0.3
 ps <- 0.1
 pa <- 0.2
-add00 <- 0.1
+add00 <- c(0.4721719, 7.3144796) # beta distribution parameters derived from parameterizingTheModel.Rmd.
 lose01 <- 0.3 
 add10 <- 0.2
-lose11 <- 0.2
+lose11 <- c(0.3283134, 0.3062181) # beta distribution parameters derived from parameterizingTheModel.Rmd.
 histMultiplier <- 1.2
-
-# Results storage
-den.orig <- matrix(NA, n.rep)
-den.rewired <- matrix(NA, n.rep)
-mean.deg.orig <- matrix(NA, n.rep)
-mean.deg.rewired <- matrix(NA, n.rep)
-# assort
-# clust
 
 for(zz in 1:n.rep){
   # Generate a random starting network
@@ -68,6 +56,8 @@ for(zz in 1:n.rep){
   
   # Removal/perturbation and rewiring
   rewired.list <- remove.network.node(network = network.history[[burn.in]], 
+                                      previous = network.history[[burn.in-1]],
+                                      histMultiplier = histMultiplier,
                                       n.removed = 1, pm = rnorm(1, pm), ps = rnorm(1, ps), pa = rnorm(1, pa))
   rewired.network <- rewired.list$network # the actual rewired network
   rewired.del <- rewired.list$del # removed individuals. Used for amending history matrices in order to continue baseline dynamics moving forward
