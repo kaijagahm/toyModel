f917a17dfb551365e3f4a4e6df5a4fb7f81f298a
Kaija <kaija.gahm@gmail.com>
2022-08-18 16:18:20 -0700

    instead of reinventing the wheel, just make an extra "baseline dynamics" network, which we can later remove a node from. Duh...

--- modelFunction_rewiring.R
+++ modelFunction_rewiring.R
@@ -21,59 +21,58 @@ runModel <- function(N = 50, # Nodes in the network
                      lose11 = c(0.3283134, 0.3062181), # beta distribution parameters derived from parameterizingTheModel.Rmd.
                      histMultiplier = 1.2,
                      doRemoval = TRUE){
-    # Generate a random starting network
-    network.orig <- sna::rgraph(N, tprob = edge.prob, 
-                                mode = "graph") # gives undirected graph, already symmetrized.
-    
-    # Run the baseline model
-    ## set up the history
-    network.history <- vector(mode = "list", length = burn.in)
-    network.history[[1]] <- matrix(0, N, N) # blank network so we can look 2 timesteps back
-    network.history[[2]] <- network.orig # baseline network
-    
-    ## run the loop, starting at index 3
-    for(i in 3:burn.in){
-      output <- update.network(ind = i, network.history, add00 = add00, 
-                               add10 = add10, lose01 = lose01, lose11 = lose11)
-      network.history[[i]] <- output # update history
-    }
-    
-    if(doRemoval == FALSE){
-      return(network.history)
-    }
-    
-    if(doRemoval == TRUE){
-      # Removal/perturbation and rewiring
-      rewired.list <- remove.and.rewire(network = network.history[[burn.in]], 
-                                          previous = network.history[[burn.in-1]],
-                                          histMultiplier = histMultiplier,
-                                          n.removed = 1, pm = rnorm(1, pm), 
-                                          ps = rnorm(1, ps), pa = rnorm(1, pa))
-      rewired.network <- rewired.list$network # the actual rewired network
-      rewired.del <- rewired.list$del # removed individuals. Used for amending history matrices in order to continue baseline dynamics moving forward
-      
-      # Continue baseline dynamics following removal
-      ## Set up a list: the old network history, the rewired network, and some placeholders for the burnout
-      history <- append(network.history, list(rewired.network))
-      history <- append(history, rep(NA, burn.out)) # add spots for the burn out
-      
-      for(i in (burn.in+2):length(history)){
-        output <- update.network(ind = i, history, add00 = add00, 
-                                 add10 = add10, lose01 = lose01, lose11 = lose11)
-        history[[i]] <- output # update history
-      }
-      
-      # Make sure column names are assigned so we can keep track of the nodes!
-      history <- lapply(history, function(x){
-        colnames(x) <- paste0("v", 1:ncol(x))
-        return(x)
-      })
-      
-      # Now we have to remove the deleted node(s) from all networks following the removal.
-      for(i in (burn.in+1):length(history)){
-        history[[i]] <- history[[i]][-rewired.del,]
-        history[[i]] <- history[[i]][,-rewired.del]
-      }
-      return(history)
-    }
+  # ARGUMENT CHECKS
+  # XXX update these.
+  
+  # RANDOM STARTING NETWORK
+  network.orig <- sna::rgraph(N, tprob = edge.prob, 
+                              mode = "graph") # gives undirected graph, already symmetrized.
+  
+  # BURN-IN
+  ## Empty list to hold networks
+  network.history <- vector(mode = "list", length = burn.in + 1)
+  ## Create pre-history
+  network.history[[1]] <- matrix(0, N, N) # blank network to enable looking 2 timesteps back
+  network.history[[2]] <- network.orig # baseline network
+  
+  ## Create the rest of the burn-in-period networks, starting at the 3rd element.
+  ## The last element (network.history[[burn.in+1]]) is the one that will be modified by removing node(s).
+  for(i in 3:burn.in+1){
+    output <- update.network(ind = i, network.history, add00 = add00, 
+                             add10 = add10, lose01 = lose01, lose11 = lose11)
+    network.history[[i]] <- output # update history
+  }
+  
+  # Removal/perturbation and rewiring
+  rewired.list <- remove.and.rewire(network = network.history[[burn.in]], 
+                                    previous = network.history[[burn.in-1]],
+                                    histMultiplier = histMultiplier,
+                                    n.removed = 1, pm = rnorm(1, pm), 
+                                    ps = rnorm(1, ps), pa = rnorm(1, pa))
+  rewired.network <- rewired.list$network # the actual rewired network
+  rewired.del <- rewired.list$del # removed individuals. Used for amending history matrices in order to continue baseline dynamics moving forward
+  
+  # Continue baseline dynamics following removal
+  ## Set up a list: the old network history, the rewired network, and some placeholders for the burnout
+  history <- append(network.history, list(rewired.network))
+  history <- append(history, rep(NA, burn.out)) # add spots for the burn out
+  
+  for(i in (burn.in+2):length(history)){
+    output <- update.network(ind = i, history, add00 = add00, 
+                             add10 = add10, lose01 = lose01, lose11 = lose11)
+    history[[i]] <- output # update history
+  }
+  
+  # Make sure column names are assigned so we can keep track of the nodes!
+  history <- lapply(history, function(x){
+    colnames(x) <- paste0("v", 1:ncol(x))
+    return(x)
+  })
+  
+  # Now we have to remove the deleted node(s) from all networks following the removal.
+  for(i in (burn.in+1):length(history)){
+    history[[i]] <- history[[i]][-rewired.del,]
+    history[[i]] <- history[[i]][,-rewired.del]
+  }
+  return(history)
 }
